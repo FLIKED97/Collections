7542a67ac7ae043694d15cecd39ed58bbed9b744
FLIKED97 <91269654+FLIKED97@users.noreply.github.com>
2025-07-21 20:16:18 +0300

    Лямбда вирази сортування ліста + лямбду можна записати в змінну

--- src/TestLambdaFirst.java
+++ src/TestLambdaFirst.java
@@ -1,24 +1,44 @@
-interface Executable{
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
-    int execute(int x, int y);
-}
-class Runner{
-    public void run(Executable e){
-      int a =  e.execute(10, 9);
-        System.out.println(a);
-    }
-}
+//interface Executable{
+//
+//    int execute(int x, int y);
+//}
+//class Runner{
+//    public void run(Executable e){
+//      int a =  e.execute(10, 9);
+//        System.out.println(a);
+//    }
+//}
 public class TestLambdaFirst {
     public static void main(String[] args) {
-        Runner runner = new Runner();
-        runner.run(new Executable() {
-            @Override
-            public int execute(int x, int y) {
-                System.out.println("Hello");
+//        Runner runner = new Runner();
+//        runner.run(new Executable() {
+//            @Override
+//            public int execute(int x, int y) {
+//                int a = 1;
+//                System.out.println("Hello");
+//
+//                return x+y+a;
+//            }
+//        });
+//
+//        final int a = 1; //Передаються тільки константи в лямбду!
+//        runner.run((x, y) -> x+y+a);// Немає своєї області видимості, тобто там де була створена то і бачить
 
-                return x+y;
-            }
-        });
-        runner.run((x, y) -> x+y);
+        List<String> list = new ArrayList<>();
+        list.add("Sasha");
+        list.add("dddd");
+        list.add("ad");
+
+        Comparator<String> comparator = (str1, str2) ->  {
+            if (str1.length() > str2.length()) return 1;
+            else if (str1.length() < str2.length()) return -1;
+            else return 0;
+        };
+        list.sort(comparator);
+        System.out.println(list);
     }
 }
